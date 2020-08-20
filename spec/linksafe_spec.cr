9b886bfae95b9df3189299529a04e99abf2a5a2f
Jonathan Silverman <jsilverman2@gmail.com>
2020-08-19 22:01:25 -0600

    working linksafe

---
+++ spec/linksafe_spec.cr
@@ -0,0 +1,17 @@
+require "./spec_helper"
+
+describe Linksafe do
+  # TODO: Write tests
+
+  it "works" do
+    Linksafe.kosher?("http://www.porn.com").should eq(false)
+    Linksafe.kosher?("http://www.google.com").should eq(true)
+    Linksafe.kosher?("www.porn.com").should eq(false)
+    Linksafe.kosher?("www.google.com").should eq(true)
+  end
+
+  it "can detect links in messages and return if kosher" do
+    Linksafe.kosher_string!("This is a link to www.google.com").should eq("This is a link to www.google.com")
+    Linksafe.kosher_string!("This is a link to www.google.com and www.porn.com").should eq("This is a link to www.google.com and [link removed]")
+  end
+end
